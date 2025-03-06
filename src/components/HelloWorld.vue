dba27f20de8dfad2401a0135f41e71c8f7f36fd3
zzimong1 <whrjsgh123@gmail.com>
2025-03-06 08:03:01 +0000

    v3

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -80,6 +80,7 @@
         </a>
       </li>
     </ul>
+    <h1> My Second Release! </h1>
   </div>
 </template>
 
@@ -88,7 +89,7 @@ export default {
   name: 'HelloWorld',
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      msg: 'It's me SexyBoy ZZIMONG'
     }
   }
 }
